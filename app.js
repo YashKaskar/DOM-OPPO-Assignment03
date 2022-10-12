4c61ed1b5563e5bf8963cc8d7c9d941af73fe62d
Yash Kaskar <yash.kaskar99@gmail.com>
2022-10-12 11:25:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// Webiste Name: [Oppo](https://www.oppo.com/in/)
+// ### Tasks
+
+//       Change the description colour black to orange
+
+
+
+
+// Write your code here:
+
+document.querySelector('.desc');
+<h3 class="desc">​…​</h3>​
+
+document.querySelector('.desc').style.color = 'orange';
+'orange'
